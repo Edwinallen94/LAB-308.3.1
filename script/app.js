8cf1a07c8e805ac28607c51bbe53187d7f256ed6
Edwin <Edwinallen94@aol.com>
2024-09-10 14:49:51 -0400

    Started the first for loop with the divison of 3 / 5 that prints fizzbuzz

--- script/app.js
+++ script/app.js
@@ -5,3 +5,12 @@
 // If a number is divisible by 5, log “Buzz.”
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
+
+// Created the for loop that console.log's FIZZBUZZ statment
+for (let i = 1; i <= 100; i++) {
+  //started from 1 but maxed to 100 with increments
+  if (i % 3 === 0 && i % 5 === 0) {
+    // making 3 with it's divison and 5 to say FizzBuzz with it's divison
+    console.log("FizzBuzz"); //will be the final statement in the node console
+  }
+}
