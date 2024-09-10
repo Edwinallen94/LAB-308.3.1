b91fd35b81803cdf4001ba08ddf8178a7c3bd0d2
Edwin <Edwinallen94@aol.com>
2024-09-10 14:55:38 -0400

    Output prints FIZZ, BUZZ AND FIZZBUZZ throughout the console

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+// FIZZ BUZZ
+
 // To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
 // Accomplish the following:
 // Loop through all numbers from 1 to 100.
@@ -12,5 +14,13 @@ for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     // making 3 with it's divison and 5 to say FizzBuzz with it's divison
     console.log("FizzBuzz"); //will be the final statement in the node console
+  } else if (i % 3 === 0) {
+    // secluded the 3 to only print fizz once its divisable by 3
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    // secluded the 5 to only print buzz once its diviable by 5
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
 }
